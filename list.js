b7a1dee11e87490bbae811dc1c0d6dad71c1103f
Shawn Taylor <shawn@mapsandapps.net>
2018-04-22 20:02:48 -0400

    UMCH map

--- list.js
+++ list.js
@@ -3,13 +3,15 @@ var list = [
     name: 'Johns Homestead',
     id: 'johns-homestead',
     latitude: 33.8310489,
-    longitude: -84.246261
+    longitude: -84.246261,
+    geojsonio: 'http://geojson.io/#id=gist:anonymous/ac4e24e1b474df5dac41080189200dc1&map=16/33.8312/-84.2453'
   },
   {
     name: 'Proctor Creek Greenway',
     id: 'proctor-creek-greenway',
     latitude: 33.77200621120143,
-    longitude: -84.42974120378494
+    longitude: -84.42974120378494,
+    geojsonio: 'http://geojson.io/#id=gist:mapsandapps/989a41e8400f17b60848a1c70dda5b04&map=15/33.7868/-84.4444'
   },
   {
     name: 'Southside Park',
@@ -24,5 +26,13 @@ var list = [
     latitude: 33.72172061276281,
     longitude: -84.56431835889816,
     geojsonio: 'http://geojson.io/#id=gist:mapsandapps/9f8d4e3ffe0441a89c9a867dbbd8ca16&map=18/33.72143/-84.56522'
+  },
+  {
+    name: 'UMCH',
+    id: 'umch',
+    latitude: 33.76676536958019,
+    longitude: -84.2778691649437,
+    geojsonio: 'http://geojson.io/#id=gist:mapsandapps/265224cdc240e32648b3eff09437673b&map=17/33.76690/-84.27942'
   }
 ];
+// thomson park http://geojson.io/#id=gist:mapsandapps/ee9b23fbc914a747c4e57c3b8eddd9c3&map=17/33.81056/-84.31863
